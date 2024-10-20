929388af61a7a1b86034d42a20e08de8267974b9
aksel26 <kevinxkim2023@gmail.com>
2024-10-20 18:53:03 +0900

    add font-family

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -7,8 +7,10 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+
 const theme = createTheme({
   defaultRadius: "md",
+  fontFamily: "NanumSquareNeo, sans-serif",
 });
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
